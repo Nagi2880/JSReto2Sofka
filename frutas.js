3214d8efa49b58119adf16646018a979abf69894
Nagi2880 <alexanderamaral17@gmail.com>
2023-02-25 16:59:25 -0300

    RetoSofka

--- frutas.js
+++ frutas.js
@@ -1,15 +1,21 @@
+// Obtener la lista ordenada de frutas
 const fruitsList = document.getElementById("fruits-list");
 
+// Obtener todos los elementos de la lista
 const fruits = fruitsList.getElementsByTagName("li");
 
-for (let i = 0; i < fruits.length; i++) {
-  if (i % 2 === 0) {
-    fruits[i].classList.add("fruit-even");
-  } else {
-    fruits[i].classList.add("fruit-odd");
+// Función para resaltar los elementos pares
+function highlightEven() {
+  for (let i = 0; i < fruits.length; i++) {
+    if (i % 2 === 0) {
+      fruits[i].classList.add("fruit-even");
+      fruits[i].classList.remove("fruit-odd");
+    } else {
+      fruits[i].classList.add("fruit-odd");
+      fruits[i].classList.remove("fruit-even");
+    }
   }
 }
-
 
 // Función para resaltar los elementos impares
 function highlightOdd() {
