9ea9a1cb7b9ee939df1fa894da8275a8a5b8e3dc
Nagi2880 <alexanderamaral17@gmail.com>
2023-02-25 16:57:08 -0300

    RetoSofka

--- frutas.js
+++ frutas.js
@@ -9,3 +9,24 @@ for (let i = 0; i < fruits.length; i++) {
     fruits[i].classList.add("fruit-odd");
   }
 }
+
+
+// Función para resaltar los elementos impares
+function highlightOdd() {
+  for (let i = 0; i < fruits.length; i++) {
+    if (i % 2 === 0) {
+      fruits[i].classList.add("fruit-odd");
+      fruits[i].classList.remove("fruit-even");
+    } else {
+      fruits[i].classList.add("fruit-even");
+      fruits[i].classList.remove("fruit-odd");
+    }
+  }
+}
+
+// Agregar los controladores de eventos para los botones
+const buttonEven = document.getElementById("button-even");
+buttonEven.addEventListener("click", highlightEven);
+
+const buttonOdd = document.getElementById("button-odd");
+buttonOdd.addEventListener("click", highlightOdd);
